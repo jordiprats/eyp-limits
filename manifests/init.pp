70c554a2cb3205e72e95ebb8bdfa5b2b981ff13c
Jordi Prats <jprats@systemadmin.es>
2015-09-03 00:11:15 +0200

    inicial

---
+++ manifests/init.pp
@@ -0,0 +1,22 @@
+class limits inherits params {
+
+  package { $limits::params::package:
+    ensure => 'installed',
+  }
+
+  #/etc/security/limits.conf
+  concat { $limits::params::limits_conf:
+    ensure => 'present',
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    require => Package[$limits::params::package],
+  }
+
+  concat::fragment{ "limits.conf header":
+    target  => $limits::params::limits_conf,
+    order => '00',
+    content => template("${module_name}/limits_header.erb"),
+  }
+
+}
---
+++ manifests/params.pp
@@ -0,0 +1,17 @@
+class limits::params {
+
+  $limits_conf="/etc/security/limits.conf"
+
+	case $::osfamily
+	{
+		'redhat':
+    {
+      $package="pam"
+		}
+		'Debian':
+		{
+      $package="libpam-modules"
+		}
+		default: { fail("Unsupported OS!")  }
+	}
+}
