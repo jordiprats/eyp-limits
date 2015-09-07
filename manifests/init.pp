209dfd4ccc05072ce5b935d64af9f71764299321
Jordi Prats <jprats@systemadmin.es>
2015-09-07 11:15:15 +0200

    aliniacions

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class limits inherits limits::params {
+class limits inherits params {
 
   package { $limits::params::package:
     ensure => 'installed',
@@ -6,16 +6,16 @@ class limits inherits limits::params {
 
   #/etc/security/limits.conf
   concat { $limits::params::limits_conf:
-    ensure => 'present',
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
     require => Package[$limits::params::package],
   }
 
-  concat::fragment{ 'limits.conf header':
+  concat::fragment{ "limits.conf header":
     target  => $limits::params::limits_conf,
-    order => '00',
+    order   => '00',
     content => template("${module_name}/limits_header.erb"),
   }
 
