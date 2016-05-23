fac9ef9fe8d516057e7f1771ed9cd7c7cce1c16a
Jordi Prats <jprats@systemadmin.es>
2016-05-23 14:43:10 +0200

    afegit domain per si de cas

--- manifests/limit.pp
+++ manifests/limit.pp
@@ -1,6 +1,6 @@
 define limits::limit ( $item, $value, $domain=$name, $type='-' ) {
   #
-  concat::fragment{ "limits.conf ${name} ${type} ${item} ${value}":
+  concat::fragment{ "limits.conf ${domain} ${name} ${type} ${item} ${value}":
     target  => $limits::params::limits_conf,
     order   => '01',
     content => template("${module_name}/limit.erb"),
